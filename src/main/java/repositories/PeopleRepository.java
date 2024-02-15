b4746744005fb947ed7848e15c727173466afdee
Giorgi1337 <kvacabaya02@gmail.com>
2024-02-15 12:28:34 +0400

    testing RESTAPI

---
+++ src/main/java/repositories/PeopleRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+}
